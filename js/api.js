5b3d358cf4b9039c53abc596e596944ba86af526
Cristiano Mattos <mateuscmattos@gmail.com>
2026-07-27 21:19:46 -0300

    feat: add weather icons and improve weather cards UI

--- js/api.js
+++ js/api.js
@@ -1,3 +1,5 @@
+const API_KEY = 'e78d61daf65a36ac30cd35117f1d4b0d'
+
 export async function getCities() {
     const response = await fetch("https://servicodados.ibge.gov.br/api/v1/localidades/municipios");
     const data = await response.json();
@@ -5,7 +7,7 @@ export async function getCities() {
 }
 
 async function getCoordinates(city) {
-    const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=e78d61daf65a36ac30cd35117f1d4b0d`);
+    const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`);
     const data = await response.json();
     const {lat, lon} = data[0];
     return {latitude: lat, longitude: lon};
@@ -13,7 +15,7 @@ async function getCoordinates(city) {
 
 export async function getWeatherData(city) {
     const {latitude, longitude} = await getCoordinates(city);
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&lang=pt_br&appid=e78d61daf65a36ac30cd35117f1d4b0d`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&lang=pt_br&appid=${API_KEY}`);
     const data = await response.json();
 
     const {description, icon} = data.weather[0];
