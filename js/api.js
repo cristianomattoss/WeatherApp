5a84240f1652b4eef157c99ff07e8c3f072a57da
Cristiano Mattos <mateuscmattos@gmail.com>
2026-07-27 22:18:52 -0300

    feat: include state in city search

--- js/api.js
+++ js/api.js
@@ -3,11 +3,14 @@ const API_KEY = 'e78d61daf65a36ac30cd35117f1d4b0d'
 export async function getCities() {
     const response = await fetch("https://servicodados.ibge.gov.br/api/v1/localidades/municipios");
     const data = await response.json();
-    return data.map(city => city.nome);
+    return data
+    .filter(city => city.microrregiao !== null)
+    .map(city => `${city.nome} - ${city.microrregiao.mesorregiao.UF.sigla}`);
 }
 
 async function getCoordinates(city) {
-    const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`);
+    const [nameCity, state] = city.split(" - ");
+    const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${nameCity},${state},BR&limit=1&appid=${API_KEY}`);
     const data = await response.json();
     const {lat, lon} = data[0];
     return {latitude: lat, longitude: lon};
